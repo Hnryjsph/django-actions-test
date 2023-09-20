b1026b70de3ac4016abe1c7d0ffe57a3069719ed
Hnry jsph <hnryjsph@outlook.com>
2023-09-20 17:20:06 +0300

    nc

--- myapp/views.py
+++ myapp/views.py
@@ -3,9 +3,10 @@ import waffle
 from waffle.decorators import waffle_flag
 from waffle.models import Flag
 
+
 flag, created = Flag.objects.get_or_create(name='new_feature', defaults={'everyone': True})
 
-#Hello
+
 @waffle_flag('new_feature')
 def new_feature_view(request):
     context = {}
