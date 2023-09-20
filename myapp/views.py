3e3d1776b26b4625ac422868c4fb3e647195f5e9
Hnry jsph <hnryjsph@outlook.com>
2023-09-20 19:07:05 +0300

    nc

--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render
 import waffle
-from waffle.decorators import waffle_flag
-from waffle.models import Flag
 
 
-flag, created = Flag.objects.get_or_create(name='new_feature', defaults={'everyone': True})
-
-
-@waffle_flag('new_feature')
 def new_feature_view(request):
     context = {}
     if waffle.flag_is_active(request, 'button'):
