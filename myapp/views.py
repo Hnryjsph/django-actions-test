efcbd8440c5e064a9ea5b0e5b9fafe58c131a54f
Hnry jsph <hnryjsph@outlook.com>
2023-09-20 19:21:55 +0300

    nc

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-
-
+import git
 
 def new_feature_view(request):
     context = {}
@@ -10,3 +9,14 @@ def new_feature_view(request):
 
 
     return render(request, 'simple.html', context)
+
+def webhook(request):
+    if request.method == 'POST':
+        repo = git.Repo('https://github.com/Hnryjsph/django-actions-test.git')
+        origin = repo.remotes.origin
+        origin.pull()
+        return "Updated SERVER", 200
+    else:
+        return 'Wrong event type', 400
+
+
