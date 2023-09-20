8c6e674891c716f18e22061d7ae9c7c00202e7c3
Hnry jsph <hnryjsph@outlook.com>
2023-09-20 19:57:44 +0300

    ref[view.py]: Adding update work flow

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,6 @@ from .views import new_feature_view, webhook
 
 urlpatterns = [
     path('', new_feature_view, name='new_feature_view'),
-    path('update-server', webhook, name='webhook'),
+    path('update-server/', webhook, name='webhook'),
     # ... other URL patterns
 ]
