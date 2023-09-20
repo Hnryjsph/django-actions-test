d0e9d10e917f596e54935c35a535a876f50e7582
Hnry jsph <hnryjsph@outlook.com>
2023-09-20 19:09:03 +0300

    nc

--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
-import waffle
+
 
 
 def new_feature_view(request):
     context = {}
-    if waffle.flag_is_active(request, 'button'):
 
-        context['show'] = True
-    else:
-        context['show'] = False
+
+    context['show'] = True
+
 
     return render(request, 'simple.html', context)
